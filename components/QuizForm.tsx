bbb12af016fc5e3b0d38523fddec3a172f1a1f1c
openmpy <suhwan@kakao.com>
2024-09-03 12:28:05 +0900

    feat: Add Question component to render quiz questions on QuizForm

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -3,6 +3,7 @@
 import { getQuizQuestions, getStates } from "@/utils/functions";
 import { IQuestion } from "@/utils/types";
 import { useState } from "react";
+import Question from "./Question";
 import SelectState from "./SelectState";
 
 export default function QuizForm() {
@@ -18,6 +19,16 @@ export default function QuizForm() {
   const startQuiz = () => {
     setQuestions(getQuizQuestions(state));
     setQuizStarted(true);
+  };
+
+  const quizEnd = () => {
+    setQuizEnded(true);
+  };
+
+  const nextQuestion = () => {
+    setCurrentQuestion(currentQuestion + 1);
+    setAnswers([...answers, currentAnswer]);
+    setAnswered(false);
   };
 
   return (
@@ -40,6 +51,36 @@ export default function QuizForm() {
           >
             연습 테스트 시작
           </button>
+        </div>
+      )}
+      {quizStarted && !quizEnded && (
+        <div>
+          <Question
+            question={questions[currentQuestion]}
+            getAnswer={(answer) => {
+              setCurrentAnswer(answer);
+              setAnswered(true);
+            }}
+            withId={false}
+            checkEnabled={false}
+          />
+          <div>
+            <div>
+              문제 {currentQuestion + 1} / {questions.length}
+            </div>
+            {answered && (
+              <button
+                className="my-4 px-8 py-2 text-white float-right rounded-lg shadow bg-blue-600 hover:bg-blue-500"
+                onClick={() =>
+                  currentQuestion > questions.length - 1
+                    ? quizEnd()
+                    : nextQuestion()
+                }
+              >
+                {currentQuestion > questions.length - 1 ? "완료" : "다음"}
+              </button>
+            )}
+          </div>
         </div>
       )}
     </div>
